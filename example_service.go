e7fcc4ae22116f8c46e504299a2ed0b8f7a53a3d
muxueqz <zhangmingyuan240@gmail.com>
2024-02-22 21:06:21 +0800

    完成第2个和第3个

--- example_service.go
+++ example_service.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 const (
 	bigBrickSize   = 5
 	smallBrickSize = 2
@@ -22,10 +28,34 @@ func CanFillGap(smallQuantity, bigQuantity, totalGap int) bool {
 
 // given a string, encode it using length encoding
 func LengthEncode(input string) string {
-	return ""
+	r := make([]string, 0)
+	k := ""
+	count := 1
+	ss := strings.Split(input, "")
+	for i := 0; i < len(ss); i++ {
+    v := ss[i]
+		if (k != v && k != "") {
+			r = append(r, fmt.Sprintf("%d", count-1)+k)
+			count = 1
+		}
+		if i == len(ss)-1 {
+			r = append(r, fmt.Sprintf("%d", count)+v)
+		}
+		k = v
+		count += 1
+	}
+  return strings.Join(r, "")
 }
 
 // given an integer, reverse it
 func Reverse(input int) int {
-	return 0
+	s := fmt.Sprintf("%d", input)
+	ss := strings.Split(s, "")
+	ss_ := make([]string, len(s))
+	for i := len(s) - 1; i >= 0; i-- {
+		ss_ = append(ss_, ss[i])
+	}
+	r := strings.Join(ss_, "")
+	i, _ := strconv.Atoi(r)
+	return i
 }
